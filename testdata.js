b3d976bb0443ee982710ef8deb0363ecf4bdc2f5
nahlagadeem <nahla.gadeem@gmail.com>
2022-03-23 18:25:24 +0300

    first commit

---
+++ testdata.js
@@ -0,0 +1,6 @@
+var dataSet = [
+    [ "Nahla Abubaker", "0577722616", "nahla@mail.com", "19/09/1992", "KSA", "Disney" ],
+    [ "Ahmed Issam", "0577722611", "ahmed@mail.com", "31/01/1992", "Qatar", "NASA" ],
+    [ "Khadija Abdalgader", "0577722666", "khadija@mail.com", "06/09/1992", "Dubai", "Itisalat" ],
+    [ "Sawsan Alrasheed", "0577722661", "sawsan@mail.com", "10/09/1992", "KSA", "University" ]
+];
